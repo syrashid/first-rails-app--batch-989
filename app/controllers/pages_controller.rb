0b9b595e463d4ca1eec07681e1d4809ef5dadd6a
Sy Rashid <sy.rashid.11@gmail.com>
2022-10-24 11:20:31 +0200

    FEAT: Finish lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home; end
-  def about; end
+
+  def about
+    @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+    if params[:member]
+      @members = @members.select { |member| member.start_with? params[:member]}
+    end
+  end
+
   def contact; end
 end
